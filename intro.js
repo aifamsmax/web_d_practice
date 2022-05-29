b227a1b5e8f252779f2fe96268825476dc847ba5
aifamsmax <aifamsmax@gmail.com>
2022-05-29 11:31:59 +0530

    my files

---
+++ intro.js
@@ -0,0 +1,8 @@
+//print
+console.log("hello js");
+//declare variable
+let a;
+console.log(a);//undefined
+a = 10;
+console.log(a);
+//variable types : primitive types : number, string , boolean null;
